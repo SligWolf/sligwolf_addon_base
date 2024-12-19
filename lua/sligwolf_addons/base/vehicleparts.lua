9ce92be5dbe9753fd13846b7f186595333d5ade1
SligWolf <sligwolf@gmail.com>
2024-12-19 20:58:58 +0100

    Fixed Keepupright parameters

--- lua/sligwolf_addons/base/vehicleparts.lua
+++ lua/sligwolf_addons/base/vehicleparts.lua
@@ -371,8 +371,8 @@ local function CreateAdvBallsocket(ent, parent, constraintInfos)
 	return constraintEnt
 end
 
-local function CreateKeepupright(ent, parent, constraintInfos)
-	local constraintEnt = LIBConstraints.Keepupright(ent, parent, constraintInfos)
+local function CreateKeepupright(ent, constraintInfos)
+	local constraintEnt = LIBConstraints.Keepupright(ent, constraintInfos)
 	return constraintEnt
 end
 
